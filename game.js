7d522644cfc2d9a76b098414b04b6f2e3386a2d5
Ramses A Aquilina <3redjumper3@gmail.com>
2025-12-03 16:51:50 +0100

    Making progress on jump and falling mechanic.

--- game.js
+++ game.js
@@ -2,49 +2,31 @@ import { Platform } from "platform";
 import { Player } from "player";
 import player from "./player";
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
 }
 
 let canvasWidth = 300;
 let canvasHeight = 400;
-let character = new player(canvasWidth * 0.5, canvasHeight * 0.97, 10, 10);
+let character = new player(canvasWidth * 0.5, canvasHeight * 0.87, 50, 50);
 let floor = 400;
-let camera = {
-  x: canvasWidth * 0.5,
-  y: canvasHeight * 0.5,
-  w: canvasWidth,
-  h: canvasHeight,
-  zoom: 4,
-  worldW: 2000,
-  worldH: 2000,
-};
+let platforms = [new platform("Breaking", 50, 150, 50, 10)];
 
 function draw() {
   background(170, 170, 255);
 
-  camera.x = character.x + character.w * 0.5;
-  camera.y = character.y + character.h * 0.5;
-
-  const halfViewW = (canvasWidth / camera.zoom) * 0.5;
-  const halfViewH = (canvasHeight / camera.zoom) * 0.5;
-
-  camera.x = Math.max(camera.x, halfViewW);
-  camera.y = Math.max(camera.y, halfViewH);
-  camera.x = Math.min(camera.x, camera.worldW - halfViewW);
-  camera.y = Math.min(camera.y, camera.worldH - halfViewH);
-
-  push();
-  translate(canvasWidth * 0.5, canvasHeight * 0.5);
-  scale(camera.zoom);
-  translate(-camera.x, -camera.y);
-
   character.draw();
+  push();
   stroke("magenta");
   strokeWeight(4);
-  line(0, floor, camera.worldW, floor);
-
+  line(0, floor, floor, canvasHeight);
   pop();
+  for (let i in platforms) {
+    platforms[i].draw();
+  }
 }
 let l = false;
 while (l == true) {
@@ -53,3 +35,14 @@ while (l == true) {
   }
 }
 const platformtypes = ["Normal", "Moving", "Breaking"];
+
+//jumping
+function keyPressed() {
+  platforms[0].y += 20;
+  floor += 1;
+  for (let i = 0; i < 15; i++) {
+    console.log("fall");
+    platforms[0].y -= 2;
+    sleep(500000);
+  }
+}
