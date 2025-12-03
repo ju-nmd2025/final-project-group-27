bfa0f60bfa4556d51eea5b7cf43f02a2ffb73d50
Ramses A Aquilina <3redjumper3@gmail.com>
2025-12-03 14:57:44 +0100

    Added camera which centers on chicken and respects world bounds

--- game.js
+++ game.js
@@ -8,19 +8,48 @@ function setup() {
 
 let canvasWidth = 300;
 let canvasHeight = 400;
-let character = new player(canvasWidth*0.5,canvasHeight*0.97,10,10); 
+let character = new player(canvasWidth * 0.5, canvasHeight * 0.97, 10, 10);
 let floor = 400;
+let camera = {
+  x: canvasWidth * 0.5,
+  y: canvasHeight * 0.5,
+  w: canvasWidth,
+  h: canvasHeight,
+  zoom: 4,
+  worldW: 2000,
+  worldH: 2000,
+};
+
 function draw() {
   background(170, 170, 255);
+
+  camera.x = character.x + character.w * 0.5;
+  camera.y = character.y + character.h * 0.5;
+
+  const halfViewW = (canvasWidth / camera.zoom) * 0.5;
+  const halfViewH = (canvasHeight / camera.zoom) * 0.5;
+
+  camera.x = Math.max(camera.x, halfViewW);
+  camera.y = Math.max(camera.y, halfViewH);
+  camera.x = Math.min(camera.x, camera.worldW - halfViewW);
+  camera.y = Math.min(camera.y, camera.worldH - halfViewH);
+
+  push();
+  translate(canvasWidth * 0.5, canvasHeight * 0.5);
+  scale(camera.zoom);
+  translate(-camera.x, -camera.y);
+
   character.draw();
-  stroke('magenta');
+  stroke("magenta");
   strokeWeight(4);
-  line(0,floor,canvasWidth,floor);
+  line(0, floor, camera.worldW, floor);
+
+  pop();
 }
 let l = false;
 while (l == true) {
-    if (character.isFalling == false) {
-        character.x += 10;
-    }
+  if (character.isFalling == false) {
+    character.x += 10;
+  }
 }
 const platformtypes = ["Normal", "Moving", "Breaking"];
